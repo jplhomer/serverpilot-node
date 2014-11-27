67a250bdce776a3eceed72d9a8d109e671979e0f
Josh Larson <jplhomer@gmail.com>
2014-11-26 22:28:55 -0600

    Add create app logic and tests

--- test/apps.test.js
+++ test/apps.test.js
@@ -1,7 +1,7 @@
 var should = require('should');
 var ServerPilot = require('..');
-var appId = 'CxrESKx9GXko9wen';
-var serverId;
+var sp;
+var appId, serverId;
 var options = {
     name: 'testapp',
     sysuserid: '',
@@ -9,21 +9,20 @@ var options = {
     domains: ['testapp.com']
 };
 
-function catchAppException(options) {
+function catchCreateAppException(opts) {
     return function() {
         try {
-            new ServerPilot(options);
+            sp.createApp(opts, function() {} );
         } catch (e) {
             return;
         }
-        throw new Error('No error throw by class with options: ' + JSON.stringify(options));
+        throw new Error('No error throw by class with options: ' + JSON.stringify(opts));
     };
 }
 
 describe('Apps', function() {
-    var sp;
 
-    before(function() {
+    before(function(done) {
         sp = new ServerPilot({
             clientId: process.env.SP_CLIENT_ID,
             apiKey: process.env.SP_API_KEY
@@ -32,13 +31,26 @@ describe('Apps', function() {
         // Create a dummy server
         sp.createServer('testserver', function(err, data) {
             serverId = data.data.id;
+
+            // Get the sysuserid of that server
+            sp.getSysUsers(function(err, data) {
+                var sysusers = data.data;
+
+                for (var i = sysusers.length - 1; i >= 0; i--) {
+                    if (sysusers[i].serverid === serverId) {
+                        options.sysuserid = sysusers[i].id;
+
+                        done();
+                    }
+                }
+            });
         });
     });
 
     describe('.getApps()', function() {
         it('should get all apps', function(done) {
             sp.getApps(function(err, data) {
-                if ( err ) { done(err); }
+                if (err) { return done(err); }
                 data.should.be.an.Object;
                 data.data.should.be.an.Object;
                 done();
@@ -47,13 +59,31 @@ describe('Apps', function() {
     });
 
     describe('.createApp(options)', function() {
+        it('should throw when no options passed', catchCreateAppException());
+        it('should throw when empty options passed', catchCreateAppException({}));
+        it('should throw when no name passed', catchCreateAppException({ sysuserid: options.sysuserid, runtime: options.runtime }));
+        it('should throw when no sysuserid passed', catchCreateAppException({ name: options.name, runtime: options.runtime }));
+        it('should throw when no runtime passed', catchCreateAppException({ sysuserid: options.sysuserid, name: options.name }));
+        it('should create an app', function(done) {
+            sp.createApp(options, function(err, data) {
+                if (err) { return done(err); }
 
+                data.data.name.should.eql(options.name);
+                data.data.runtime.should.eql(options.runtime);
+                data.data.domains.should.eql(options.domains);
+
+                // Set stuff to use later
+                appId = data.data.id;
+
+                done();
+            });
+        });
     });
 
     describe('.getApp(id)', function() {
         it('should get an app', function(done) {
             sp.getApp( appId, function(err, data) {
-                if ( err ) { done(err); }
+                if (err) { return done(err); }
                 data.should.be.an.Object;
                 data.data.should.be.an.Object;
                 done();
@@ -61,9 +91,11 @@ describe('Apps', function() {
         });
     });
 
-    after(function() {
+    after(function(done) {
         // Destroy the server
-        sp.deleteServer(serverId, function(err, data) {});
-    })
+        sp.deleteServer(serverId, function(err, data) {
+            done();
+        });
+    });
 
 });
