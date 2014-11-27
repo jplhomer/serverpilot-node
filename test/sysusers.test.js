f0aa757222769c3059a8a9379dce5ee023f12690
Josh Larson <jplhomer@gmail.com>
2014-11-26 23:33:04 -0600

    Add addSysUser and tests

--- test/sysusers.test.js
+++ test/sysusers.test.js
@@ -1,14 +1,34 @@
 var should = require('should');
 var ServerPilot = require('..');
+var sp;
+var serverId;
+var name = 'testuser';
+var password = '#1password';
 
-describe('Servers', function() {
-    var sp;
+function catchAddSysUser(opts) {
+    return function() {
+        try {
+            sp.addSysUser(opts, function() {} );
+        } catch(e) {
+            return;
+        }
+        throw new Error('No error throw by class for options: ' + JSON.stringify(opts));
+    };
+}
 
-    before(function() {
+describe('System Users', function() {
+
+    before(function(done) {
         sp = new ServerPilot({
             clientId: process.env.SP_CLIENT_ID,
             apiKey: process.env.SP_API_KEY
         });
+
+        // Create a dummy server
+        sp.createServer('testserver', function(err, data) {
+            serverId = data.data.id;
+            done();
+        })
     });
 
     describe('.getSysUsers()', function() {
@@ -19,6 +39,36 @@ describe('Servers', function() {
                 data.data.should.be.an.Object;
                 done();
             });
+        });
+    });
+
+    describe('.addSysUser()', function() {
+        it('should throw when no options passed', catchAddSysUser());
+        it('should throw when empty options passed', catchAddSysUser({}));
+        it('should throw when no serverid passed', catchAddSysUser({ name: name }));
+        it('should throw when no name passed', catchAddSysUser({ serverid: serverId }));
+        // it('should throw when invalid name is passed', catchAddSysUser({ name: 'Test User', serverid: serverId }));
+        it('should add sysuser', function(done) {
+            var opts = {
+                serverid: serverId,
+                name: name,
+                password: password
+            };
+
+            sp.addSysUser(opts, function(err, data) {
+                if (err) { return done(err); }
+
+                data.data.name.should.eql(opts.name);
+                data.data.serverid.should.eql(opts.serverid);
+                done();
+            });
+        });
+    });
+
+    after(function(done) {
+        // Kill dummy server
+        sp.deleteServer(serverId, function(err, data) {
+            done();
         });
     });
 });
